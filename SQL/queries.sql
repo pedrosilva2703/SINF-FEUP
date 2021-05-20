b01968c53a1bbdf9d8569c1d0ca388af09434dd1
pedrosilva2703 <78810496+pedrosilva2703@users.noreply.github.com>
2021-05-20 19:38:10 +0100

    Add files via upload

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -8,38 +8,18 @@ DuOHMMxK
 /*  Ex 1  */
 
 SET search_path TO dba34;
-SELECT tempo AS timestamp, valor_medido AS measurement FROM valor_do_sensor WHERE id_sensor=
-
-SELECT tempo AS timestamp, valor_medido AS measurement FROM valor_do_sensor AS vs, sensor AS s  WHERE vs.id_sensor=s.id_sensor
-
-SELECT tempo AS timestamp, valor_medido AS measurement FROM valor_do_sensor AS vs, sensor AS s, mote AS m, sala as sa WHERE sa.nome
-
-SELECT id_mote AS im FROM sala WHERE nome_da_sala='frigorifico'
-INTERSECT
-SELECT id_sensor FROM sensor WHERE id_mote=im AND tipo='temperatura';
 
 
-SELECT sala.nome_da_sala, sensor.id_mote, sensor.id_sensor
-FROM sala, sensor
-WHERE sala.nome_da_sala='frigorifico' AND sala.id_mote=sensor.id_mote
-
-
-SELECT * FROM sala WHERE nome_da_sala='frigorifico'
-
-SELECT * FROM sensor WHERE tipo='temperatura';
-
-SELECT * FROM valor_do_sensor WHERE tempo BETWEEN '2020-01-01' AND '2021-09-09'
-
-
-/* Ex 1 */
+/* Ex 1 VERSAO FINAL */
+SET search_path TO dba34;
 SELECT tempo AS timestamp, valor_medido AS measurement
 FROM valor_do_sensor
  JOIN sensor ON sensor.id_sensor=valor_do_sensor.id_sensor
  JOIN sala ON sala.id_mote=sensor.id_mote
 WHERE sensor.tipo = 'temperatura' AND sala.nome_da_sala = 'armazenamento' AND tempo BETWEEN '2021-05-14 13:30:44' AND '2021-05-14 13:35:44'
 
-/* Ex 2 */
-
+/* Ex 2 VERSAO TROLHA */
+/*
 (SELECT atuador.nome AS actuator, sala.nome_da_sala AS cell, estado_do_atuador.estado AS state
 FROM estado_do_atuador
  JOIN atuador ON atuador.id_atuador=estado_do_atuador.id_atuador
@@ -53,4 +33,62 @@ FROM estado_do_atuador
  JOIN sala ON sala.id_sala=atuador.id_sala
 WHERE sala.nome_da_sala = 'armazenamento' 
 ORDER BY estado_do_atuador.tempo DESC LIMIT 4)
+
+
+SET search_path TO dba34;
+SELECT *
+FROM estado_do_atuador, 
+WHERE estado_do_atuador.id_estado IN
+    (SELECT estado_do_atuador.id_atuador, MAX(estado_do_atuador.id_estado) as id_ultimos
+    FROM estado_do_atuador
+    GROUP BY estado_do_atuador.id_atuador
+    ORDER BY estado_do_atuador.id_atuador ASC)
+
+SET search_path TO dba34;
+SELECT *
+FROM estado_do_atuador, (SELECT estado_do_atuador.id_atuador, MAX(estado_do_atuador.id_estado) as id_ultimos
+    FROM estado_do_atuador
+    GROUP BY estado_do_atuador.id_atuador
+    ORDER BY estado_do_atuador.id_atuador ASC) 
+
+
+SET search_path TO dba34;
+
+SELECT original.id_atuador,original.estado
+FROM estado_do_atuador AS original, (SELECT estado_do_atuador.id_atuador AS id_ultimos_atuadores , MAX(estado_do_atuador.id_estado) AS id_ultimos_estados
+                                        FROM estado_do_atuador
+                                        GROUP BY estado_do_atuador.id_atuador
+                                        ORDER BY estado_do_atuador.id_atuador ASC) AS ultimos
+WHERE original.id_estado=ultimos.id_ultimos_estados
+*/
+
+
+
+/* EX 2 VERSAO FINAL */
+
+SET search_path TO dba34;
+
+SELECT atuador.nome AS Actuator, sala.nome_da_sala AS Cell, t1.estado AS State
+FROM ( 
+    SELECT original.id_atuador AS id_atuador, original.estado AS estado
+    FROM estado_do_atuador AS original, (SELECT estado_do_atuador.id_atuador AS id_ultimos_atuadores , MAX(estado_do_atuador.id_estado) AS id_ultimos_estados
+                                            FROM estado_do_atuador
+                                            GROUP BY estado_do_atuador.id_atuador
+                                            ORDER BY estado_do_atuador.id_atuador ASC
+                                        ) AS ultimos
+    WHERE original.id_estado=ultimos.id_ultimos_estados
+    ) AS t1
+ JOIN atuador ON atuador.id_atuador=t1.id_atuador
+ JOIN sala ON sala.id_sala=atuador.id_sala
+
+
+
+/* EX 4 */
+UPDATE regras SET regras.referencia=28 WHERE regras.id_regras=9;
+SELECT 
+
+
+
+
+
 
